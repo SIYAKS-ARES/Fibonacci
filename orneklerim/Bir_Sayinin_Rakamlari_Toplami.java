44296aa54262007a2d7adc3df709a56c041611af
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-08 17:05:22 +0300

    Sum of The Digits

--- orneklerim/Bir_Sayinin_Rakamlari_Toplami.java
+++ orneklerim/Bir_Sayinin_Rakamlari_Toplami.java
@@ -1,16 +1,21 @@
 package Orneklerim;
+
 import java.util.Scanner;
+
 public class Bir_Sayinin_Rakamlari_Toplami {
     public static void main(String[] args) {
-        // Girilen sayının rakamlar toplamını bulmaya yarayan program.
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Lütfen rakamları toplamını bulmak istediğiniz sayıyı giriniz:");
-        int sayi = scanner.nextInt();
+        System.out.print("Lütfen rakamları toplamını bulmak istediğiniz sayıyı giriniz: ");
+        String input = scanner.nextLine();
         int toplam = 0;
-        do {
-            toplam += sayi % 10;
-            sayi = (sayi - (sayi % 10)) / 10;
-        } while (sayi > 0);
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isDigit(c)) {
+                toplam += Character.getNumericValue(c);
+            }
+        }
+
         System.out.println("Girdiğiniz sayının rakamlar toplamı = " + toplam);
         scanner.close();
     }
