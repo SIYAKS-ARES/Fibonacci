194f30074493d18d8d181bb6528a07f37b3e1c8d
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-06-08 17:01:59 +0300

    Files fixed

--- AlgoSinavlari/Final2022_2023/Soru1OgrenciSecme.java
+++ AlgoSinavlari/Final2022_2023/Soru1OgrenciSecme.java
@@ -3,17 +3,21 @@ import java.util.*;
 public class Soru1OgrenciSecme {
     public static void main(String[] args) {
         List<Integer> ogrenci_numarasi = new ArrayList<>();
-        for(int i = 2023100; i <= 2023399; i++){
+        for (int i = 2023100; i <= 2023399; i++) {
             ogrenci_numarasi.add(i);
         }
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter n: ");
-        int n = scanner.nextInt();
-        Collections.shuffle(ogrenci_numarasi);
-        List<Integer> list = new ArrayList<>();
-        for (int k = 0; k < n; k++){
-            list.add(ogrenci_numarasi.get(k));
+        try {
+            System.out.print("Enter n: ");
+            int n = scanner.nextInt();
+            Collections.shuffle(ogrenci_numarasi);
+            List<Integer> list = new ArrayList<>();
+            for (int k = 0; k < n; k++) {
+                list.add(ogrenci_numarasi.get(k));
+            }
+            System.out.println(list);
+        } finally {
+            scanner.close();
         }
-        System.out.println(list);
     }
 }
--- AlgoSinavlari/Final2022_2023/Soru3DosyaAdiUzantisi.java
+++ AlgoSinavlari/Final2022_2023/Soru3DosyaAdiUzantisi.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Soru3DosyaAdiUzantisi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Bir dosya tam adı yazınız: ");
-        String dsyad = scanner.nextLine();
-        int Noki = dsyad.lastIndexOf(".");
-        String sdsyad = dsyad.substring(0, Noki);
-        String dsyuza = dsyad.substring(Noki + 1);
-        System.out.println("Dosya adı: " + sdsyad);
-        System.out.println("Dosya uzantısı: " + dsyuza);
+        try {
+            System.out.print("Bir dosya tam adı yazınız: ");
+            String dsyad = scanner.nextLine();
+            int Noki = dsyad.lastIndexOf(".");
+            String sdsyad = dsyad.substring(0, Noki);
+            String dsyuza = dsyad.substring(Noki + 1);
+            System.out.println("Dosya adı: " + sdsyad);
+            System.out.println("Dosya uzantısı: " + dsyuza);
+        } finally {
+            scanner.close();
+        }
     }
 }
